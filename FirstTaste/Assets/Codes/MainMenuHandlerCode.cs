ffcfaf34c3d5c7f39970e85db3a5548eba764993
Rushi57 <rushihanzo07@gmail.com>
2025-05-02 17:12:07 +0800

    settings Button and Credits fix working
    May Code ito

---
+++ FirstTaste/Assets/Codes/MainMenuHandlerCode.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MainMenuHandlerCode : MonoBehaviour
+{
+    public GameObject mainMenuGroup;
+    public GameObject settingsPanel;
+    public GameObject creditsPanel;
+
+    public void ShowSettings()
+    {
+        mainMenuGroup.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+    public void ShowCredits()
+    { 
+        mainMenuGroup.SetActive(false );
+        creditsPanel.SetActive(true);
+    }
+    public void BackToMainMenu()
+    { 
+        settingsPanel.SetActive(false );
+        creditsPanel.SetActive(false ) ;
+        mainMenuGroup.SetActive(true );
+    }
+}
